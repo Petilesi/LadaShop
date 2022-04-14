0846b89aa2f04684940a1c0ab1c1f5fa2bba0513
Leskó Péter <h048216@stud.u-szeged.hu>
2022-04-14 12:48:17 +0200

    uploaded shop

---
+++ app/src/main/java/com/example/ladashop/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.ladashop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void loginAsGuest(View view) {
+
+    }
+
+    public void normalLogin(View view) {
+
+    }
+
+    public void register(View view) {
+
+    }
+}
