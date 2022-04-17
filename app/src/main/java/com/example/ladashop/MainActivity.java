1491e8c9fa3b38b48e4afdcca7605740bad226ab
Leksó Péter <h048216@stud.u-szeged.hu>
2022-04-17 22:20:47 +0200

    progress update

--- app/src/main/java/com/example/ladashop/MainActivity.java
+++ app/src/main/java/com/example/ladashop/MainActivity.java
@@ -1,29 +1,142 @@
 package com.example.ladashop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getName();
+    private static final String PREF_KEY = MainActivity.class.getPackage().toString();
+    private static final int SECRET_KEY = 99;
+    private static final int RC_SIGN_IN = 123;
+
+    EditText userName;
+    EditText password;
+
+    private SharedPreferences preferences;
+    private FirebaseAuth mAuth;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userName = findViewById(R.id.userNameEditText);
+        password = findViewById(R.id.passwordEditText);
+
+        preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+        mAuth = FirebaseAuth.getInstance();
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.product_description))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
     }
 
-    public void loginAsGuest(View view) {
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if(requestCode == RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d(LOG_TAG, "firebaseAuthWithGoogle:" + account.getId());
+                firebaseAuthWithGoogle(account.getIdToken());
+            }catch (ApiException e){
+                Log.w(LOG_TAG, "Google sign in failed", e);
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Log.d(LOG_TAG, "Successful google login");
+                    startShopping();
+                }else{
+                    Log.d(LOG_TAG, "Unsuccessful google login");
+                }
+            }
+        });
+    }
+
+
+    public void loginAsGuest(View view) {
+        mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Log.d(LOG_TAG, "Anonym login successful");
+                    startShopping();
+                }else{
+                    Log.d(LOG_TAG, "Anonym login unsuccessful");
+                }
+            }
+        });
     }
 
     public void normalLogin(View view) {
+        EditText userName = findViewById(R.id.userNameEditText);
+        EditText password = findViewById(R.id.passwordEditText);
 
+        String usernameStr = userName.getText().toString();
+        String passwordStr = password.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(usernameStr, passwordStr).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Log.d(LOG_TAG, "Login successful");
+                    startShopping();
+                }else{
+                    Log.d(LOG_TAG, "Login unsuccessful");
+                }
+            }
+        });
     }
 
     public void register(View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
+    }
+
+    private void startShopping() {
+        Intent intent = new Intent(this,ListingsActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("userName", userName.getText().toString());
+        editor.putString("password", password.getText().toString());
+        editor.apply();
+        Log.i(LOG_TAG, "onPause");
     }
 }
--- app/src/main/java/com/example/ladashop/RegisterActivity.java
+++ app/src/main/java/com/example/ladashop/RegisterActivity.java
@@ -1,14 +1,86 @@
 package com.example.ladashop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+    private static final String PREF_KEY = RegisterActivity.class.getPackage().toString();
+    private static final int SECRET_KEY = 99;
+    private static final int RC_SIGN_IN = 123;
+
+    EditText usernameEditText;
+    EditText email_editText;
+    EditText passwordEditText;
+    EditText passwordCheckEditText;
+
+
+    private SharedPreferences preferences;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        usernameEditText = findViewById(R.id.userNameEditText);
+        email_editText = findViewById(R.id.email_editText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        passwordCheckEditText = findViewById(R.id.passwordCheckEditText);
+
+        preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+        String username = preferences.getString("userName", "");
+        String password = preferences.getString("password", "");
+
+        usernameEditText.setText(username);
+        passwordEditText.setText(password);
+        passwordCheckEditText.setText(password);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    public void register(View view) {
+        String username = usernameEditText.getText().toString();
+        String email = email_editText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String passwordCheck = passwordCheckEditText.getText().toString();
+
+
+        if(!password.equals(passwordCheck)){
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    startShopping();
+                }else{
+                    Toast.makeText(RegisterActivity.this, "User creation was unsuccessfull: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
+    public void cancel(View view) {
+
+    }
+
+    private void startShopping() {
+        Intent intent = new Intent(this,ListingsActivity.class);
+        startActivity(intent);
     }
 }
