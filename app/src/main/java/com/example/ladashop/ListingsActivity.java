c129f876ba456372a49c451c521065ba975d18b9
Leksó Péter <h048216@stud.u-szeged.hu>
2022-04-19 19:09:11 +0200

    progress update

--- app/src/main/java/com/example/ladashop/ListingsActivity.java
+++ app/src/main/java/com/example/ladashop/ListingsActivity.java
@@ -3,12 +3,27 @@ package com.example.ladashop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ListingsActivity extends AppCompatActivity {
+    private static final String LOG_TAG = ListingsActivity.class.getName();
+    private FirebaseUser user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listings);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            Log.d(LOG_TAG, "Authenticated user!");
+        }else {
+            Log.d(LOG_TAG, "Unauthenticated user+");
+            finish();
+        }
     }
 }
